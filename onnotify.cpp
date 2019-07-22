f46456b73ad54c1677f62a0bf012aed3a0087fad
root <root@localhost.localdomain>
2019-07-22 16:16:27 +0900

    EOSEOS

--- onnotify.cpp
+++ onnotify.cpp
@@ -4,35 +4,49 @@
 using namespace eosio;
 
 CONTRACT onnotify: public contract {
-    public:
-        using contract::contract;
+public:
+using contract::contract;
 
-        ACTION dummy() {}
+ACTION dummy() {}
 
-        [[eosio::on_notify("eosio.token::transfer")]]
-        void ontransfer(name from, name to, asset quantity, std::string memo) {
-            if(from == get_self()) {
-                outs myTable(get_self(), get_self().value);
-                if(myTable.begin() == myTable.end()) {
-                myTable.emplace(from, [&](auto& row) {
-                row.balance = quantity;
-                });
-                } else {
-                auto itr = myTable.begin();
-                myTable.modify(itr, from, [&](auto& row) {
-                row.balance += quantity;
-                });
-                }
-            } else {
-                print("you are not from");
-            }
-        }
-    private:
-        TABLE outstruct {
-            asset balance;
+[[eosio::on_notify("eosio.token::transfer")]]
+void ontransfer(name from, name to, asset quantity, std::string memo) {
+if(from == get_self()) {
+Receiver receiver(get_self(), get_self().value);
+auto itr = receiver.find(to.value);
+if(itr == receiver.end()) {
+receiver.emplace(from, [&](auto& row) {
+row.user = to;
+row.balance = quantity;
+});
+} else {
+receiver.modify(itr, from, [&](auto& row) {
+row.balance += quantity;
+});
+}
+} else {
+Sender sender(get_self(), get_self().value);
+auto itr = sender.find(from.value);
+if(itr == sender.end()) {
+sender.emplace(to, [&](auto& row) {
+row.user = from;
+row.balance = quantity;
+});
+} else {
+sender.modify(itr, to, [&](auto& row) {
+row.balance += quantity;
+});
+}
+}
+}
+private:
+TABLE trxlist {
+name user;
+asset balance;
 
-            uint64_t primary_key() const { return balance.symbol.code().raw(); }
-        };
+uint64_t primary_key() const { return user.value; }
+};
 
-        typedef multi_index<"out"_n, outstruct> outs;
+typedef multi_index<"eossender"_n, trxlist> Sender;
+typedef multi_index<"eosreceiver"_n, trxlist> Receiver;
 };
